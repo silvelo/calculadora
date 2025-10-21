742d0a5587bb997f6d5db8eace4aa70f2f349c55
Arturo Silvelo <asilvelo@trynewroads.com>
2025-10-21 14:20:23 +0200

    feat: configurar linters, hooks, commitlint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     print("=== Calculadora básica ===")
-    print("Por ahora la estructura está creada, las operaciones se agregarán después.")
+    print(
+        "Por ahora la estructura está creada, " "las operaciones se agregarán después."
+    )
+
 
 if __name__ == "__main__":
     main()
