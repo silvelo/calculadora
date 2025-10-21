64e5cebc05d83acc59b99df585e835b32895289e
Arturo Silvelo <asilvelo@trynewroads.com>
2025-10-21 13:45:15 +0200

    Añadir operación sumar y test unitario

---
+++ operaciones/suma.py
@@ -0,0 +1,3 @@
+def sumar(a, b):
+    """Devuelve la suma de a y b"""
+    return a + b
---
+++ tests/test_suma.py
@@ -0,0 +1,6 @@
+from operaciones.suma import sumar
+
+def test_sumar():
+    assert sumar(2, 3) == 5
+    assert sumar(-1, 1) == 0
+    assert sumar(0, 0) == 0
